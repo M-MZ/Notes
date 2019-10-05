75e004b44cb56df0ff3855b991ae252aa922b2f6
MMZ <mmz11493@gmail.com>
2019-10-05 21:03:21 +0800

    Set up recyclerView +ItemDecorator helper class +Dummy notes

--- app/src/main/java/com/mmz/NotesListActivity.java
+++ app/src/main/java/com/mmz/NotesListActivity.java
@@ -1,24 +1,59 @@
 package com.mmz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.mmz.adapters.NotesRecyclerAdapter;
 import com.mmz.models.Note;
+import com.mmz.utils.VerticalSpacingItemDecorator;
+
+import java.util.ArrayList;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
 
+    //UI components
+    private RecyclerView mRecyclerView;
+
+    //vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        mRecyclerView.findViewById(R.id.recyclerview);
 
-        Note note = new Note("Test", "This is tot test note model", "timestamp");
+        initRecyclerView();
+        insertFakeNotes();
+    }
 
-        Log.d(TAG, "onCreate: Note: " + note.toString() );
+    private void insertFakeNotes(){
+        for (int i = 0; i < 1000; i++){
+            Note note = new Note();
+            note.setTitle("title # " + i);
+            note.setContent("content # " + i);
+            note.setTimestamp("Jan 2019");
+            mNotes.add(note);
+        }
+        //Must have this to update adapter
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
 
 }
