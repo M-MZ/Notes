6ab6db53723353e7b669d3dc3653499cd9fa1328
MMZ <mmz11493@gmail.com>
2019-10-12 00:55:33 +0800

    initial-room-repository

---
+++ app/src/main/java/com/mmz/persistence/NoteRepository.java
@@ -0,0 +1,34 @@
+package com.mmz.persistence;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.mmz.models.Note;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context){
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note){
+
+    }
+
+    public void updateNote(Note note){
+
+    }
+
+    public LiveData<List<Note>> retrieveNotesTask(){
+        return null;
+    }
+
+    public void deleteNote(Note note){
+
+    }
+}
